db4b7592690efebbc349da48b4b82e814ed44046
Sefa Saraç <srcsfysf@gmail.com>
2024-09-04 23:12:29 +0300

    Add files via upload

---
+++ Controllers/CustomerOrder.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using YourNamespace.Models;
+
+namespace YourNamespace.Controllers
+{
+    public class CustomerOrderController : Controller
+    {
+        public IActionResult Index()
+        {
+            // Basit bir model örneği oluşturuyoruz
+            var customer = new Customer
+            {
+                Id = 1,
+                FirstName = "Jessica",
+                LastName = "Wexler",
+                Email = "jessica.wexler123456@example.com"
+            };
+
+            var orders = new List<Order>
+            {
+              new Order { OrderId = 1, ProductName = "Pc", Price = 1000, Quantity = 1 },
+              new Order { OrderId = 2, ProductName = "MobilePhone", Price = 750, Quantity = 2 },
+              new Order { OrderId = 3, ProductName = "Speaker", Price = 300, Quantity = 2 }
+            };
+
+
+            // ViewModel oluşturup view'a gönderiyoruz
+            var viewModel = new CustomerOrderViewModel
+            {
+                Customer = customer,
+                Orders = orders
+            };
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ Models/CustomerOrderViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace YourNamespace.Models
+{
+    public class CustomerOrderViewModel
+    {
+        public Customer Customer { get; set; }
+
+        public IEnumerable<Order> Orders { get; set; }
+    }
+}
